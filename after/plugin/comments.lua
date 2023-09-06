db2c9bc70b7e4bac2209e1aa518dd1398b9f08e2
JoaSt96 <joaquin.stok@hotmail.com>
2023-09-06 00:22:56 -0300

    fix comments, remap lsp saga, packer dependencies

--- after/plugin/comments.lua
+++ after/plugin/comments.lua
@@ -1,4 +1,1 @@
-require('nvim_comment').setup()
-
-vim.keymap.set('n', '<leader>;', vim.cmd.CommentToggle)
-vim.keymap.set('v', '<leader>;', vim.cmd.CommentToggle)
+require('Comment').setup()
--- lua/joacostok/packer.lua
+++ lua/joacostok/packer.lua
@@ -4,123 +4,123 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.2',
-		-- or                            , branch = '0.1.x',
-		requires = { { 'nvim-lua/plenary.nvim' } }
-	}
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-	use('nvim-treesitter/playground')
-	use('ThePrimeagen/harpoon')
-	use('mbbill/undotree')
-	use({
-		"kdheepak/lazygit.nvim",
-		-- optional for floating window border decoration
-		requires = {
-			"nvim-lua/plenary.nvim",
-		},
-	})
-	use { 'sainnhe/gruvbox-material' }
-	use({
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		requires = {
-			-- LSP Support
-			{ 'neovim/nvim-lspconfig' }, -- Required
-			{ 'williamboman/mason.nvim' }, -- Optional
-			{ 'williamboman/mason-lspconfig.nvim' }, -- Optional
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use('nvim-treesitter/playground')
+    use('ThePrimeagen/harpoon')
+    use('mbbill/undotree')
+    use({
+        "kdheepak/lazygit.nvim",
+        -- optional for floating window border decoration
+        requires = {
+            "nvim-lua/plenary.nvim",
+        },
+    })
+    use { 'sainnhe/gruvbox-material' }
+    use({
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },             -- Required
+            { 'williamboman/mason.nvim' },           -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-			-- Autocompletion
-			{ 'hrsh7th/nvim-cmp' }, -- Required
-			{ 'hrsh7th/cmp-nvim-lsp' }, -- Required
-			{ 'L3MON4D3/LuaSnip' }, -- Required
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },     -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' },     -- Required
 
-			-- Snippets
-			{ 'L3MON4D3/LuaSnip' },
-			{ 'rafamadriz/friendly-snippets' },
-		}
-	})
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
+        }
+    })
 
-	use({
-		"sindrets/diffview.nvim",
-		event = "BufRead",
-	})
+    use({
+        "sindrets/diffview.nvim",
+        event = "BufRead",
+    })
 
-	use({
-		"rmagatti/goto-preview",
-	})
+    use({
+        "rmagatti/goto-preview",
+    })
 
-	use({
-		'nvim-tree/nvim-tree.lua',
-		requires = {
-			'nvim-tree/nvim-web-devicons', -- optional
-		},
-	})
+    use({
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional
+        },
+    })
 
-	use("lukas-reineke/indent-blankline.nvim")
+    use("lukas-reineke/indent-blankline.nvim")
 
-	use { 'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons' }
+    use { 'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons' }
 
-	use { 'navarasu/onedark.nvim' }
+    use { 'navarasu/onedark.nvim' }
 
-	use {
-		'phaazon/hop.nvim',
-		branch = 'v2', -- optional but strongly recommended
-		config = function()
-			-- you can configure Hop the way you like here; see :h hop-config
-			require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
-		end
-	}
+    use {
+        'phaazon/hop.nvim',
+        branch = 'v2', -- optional but strongly recommended
+        config = function()
+            -- you can configure Hop the way you like here; see :h hop-config
+            require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+        end
+    }
 
-	use {
-		'nvim-lualine/lualine.nvim',
-		requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-	}
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
 
-	use {
-		"itchyny/vim-cursorword",
-		event = { "BufEnter", "BufNewFile" },
-		config = function()
-			vim.api.nvim_command("augroup user_plugin_cursorword")
-			vim.api.nvim_command("autocmd!")
-			vim.api.nvim_command(
-				"autocmd FileType NvimTree,lspsagafinder,dashboard,vista let b:cursorword = 0")
-			vim.api.nvim_command("autocmd WinEnter * if &diff || &pvw | let b:cursorword = 0 | endif")
-			vim.api.nvim_command("autocmd InsertEnter * let b:cursorword = 0")
-			vim.api.nvim_command("autocmd InsertLeave * let b:cursorword = 1")
-			vim.api.nvim_command("augroup END")
-		end
-	}
+    use {
+        "itchyny/vim-cursorword",
+        event = { "BufEnter", "BufNewFile" },
+        config = function()
+            vim.api.nvim_command("augroup user_plugin_cursorword")
+            vim.api.nvim_command("autocmd!")
+            vim.api.nvim_command(
+                "autocmd FileType NvimTree,lspsagafinder,dashboard,vista let b:cursorword = 0")
+            vim.api.nvim_command("autocmd WinEnter * if &diff || &pvw | let b:cursorword = 0 | endif")
+            vim.api.nvim_command("autocmd InsertEnter * let b:cursorword = 0")
+            vim.api.nvim_command("autocmd InsertLeave * let b:cursorword = 1")
+            vim.api.nvim_command("augroup END")
+        end
+    }
 
-	use { 'terrortylor/nvim-comment' }
+    use { 'numToStr/Comment.nvim'}
 
-	use { 'xiyaowong/transparent.nvim' }
+    use { 'xiyaowong/transparent.nvim' }
 
-	use {
-		"windwp/nvim-autopairs",
-		config = function() require("nvim-autopairs").setup {} end
-	}
+    use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
 
-	use { 'mg979/vim-visual-multi' }
+    use { 'mg979/vim-visual-multi' }
 
-	use { 'jose-elias-alvarez/null-ls.nvim' }
+    use { 'jose-elias-alvarez/null-ls.nvim' }
 
-	use { 'github/copilot.vim' }
+    use { 'github/copilot.vim' }
 
-	use {
-		"ray-x/lsp_signature.nvim",
-	}
+    use {
+        "ray-x/lsp_signature.nvim",
+    }
 
-	use({
-		'nvimdev/lspsaga.nvim',
-		after = 'nvim-lspconfig',
-	})
+    use({
+        'nvimdev/lspsaga.nvim',
+        after = 'nvim-lspconfig',
+    })
 
-	use {
-		'dinhhuy258/git.nvim'
-	}
+    use {
+        'dinhhuy258/git.nvim'
+    }
 end)
