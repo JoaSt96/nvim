881d6b7392f93201daf573daaf20641cfec3538d
JoaSt96 <joaquin.stok@hotmail.com>
2023-10-29 01:43:02 -0300

    copilot fix

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,3 +1,5 @@
 -- copilot remap accept
 vim.api.nvim_set_keymap('i', '<C-u>', 'copilot#Accept("<CR>")',{expr=true,silent=true})
 
+vim.g.copilot_no_tab_map = true
+vim.g.copilot_assume_mapped = true
