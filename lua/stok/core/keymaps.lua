75c1cbbc2386e8068abc1a5769266cec7efa380d
JoacoStok <joaco@n1u.app>
2024-06-06 01:39:19 -0300

    fixes

--- lua/stok/core/keymaps.lua
+++ lua/stok/core/keymaps.lua
@@ -2,38 +2,29 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>w", vim.cmd.w)
 vim.keymap.set("n", "<leader>q", vim.cmd.q)
 
--- momentario
-vim.keymap.set('n', '<leader>m', vim.cmd.LspZeroFormat)
-
-
-vim.keymap.set('n', '<leader>e', function()
-	vim.cmd.NvimTreeToggle()
-end)
-
 -- mover el texto seleccionado con J y K
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- appendear la linea anterior al final
-vim.keymap.set('n', 'J', 'mzJ`z')
+vim.keymap.set("n", "J", "mzJ`z")
 
 -- remap para no perder pegar al borrar o visar lo viejo
-vim.keymap.set('x', '<leader>p', "\"_dP")
+vim.keymap.set("x", "<leader>p", '"_dP')
 
-vim.keymap.set('n', 'Q', '<nop>')
+vim.keymap.set("n", "Q", "<nop>")
 
 --vertical split
-vim.keymap.set('n', '<leader>s', vim.cmd.vsplit)
+vim.keymap.set("n", "<leader>s", vim.cmd.vsplit)
 --horizontal split
-vim.keymap.set('n', '<leader>h', vim.cmd.split)
+vim.keymap.set("n", "<leader>h", vim.cmd.split)
 
-vim.keymap.set('n', '<leader>c', vim.cmd.bd)
+vim.keymap.set("n", "<leader>c", vim.cmd.bd)
 
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
 
-vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
-vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
-
-vim.keymap.set('n', '<leader>bn', vim.cmd.bnext)
-vim.keymap.set('n', '<leader>bb', vim.cmd.bprev)
+vim.keymap.set("n", "<leader>bn", vim.cmd.bnext)
+vim.keymap.set("n", "<leader>bb", vim.cmd.bprev)
--- lua/stok/plugins/hop.lua
+++ lua/stok/plugins/hop.lua
@@ -1,9 +1,9 @@
 return {
-	'phaazon/hop.nvim',
+	"phaazon/hop.nvim",
 	config = function()
-		require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+		require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
 		-- KEYMAPS
-		vim.keymap.set('n', "s", vim.cmd.HopChar2)
-		vim.keymap.set('n', 'S', vim.cmd.HopWord)
-	end
+		vim.keymap.set("n", "s", vim.cmd.HopChar2)
+		vim.keymap.set("n", "S", vim.cmd.HopWord)
+	end,
 }
