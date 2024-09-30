9e81178f43e976d10dc1e5089ff07b3f0819a5d5
JoaSt96 <joaquin.stok@hotmail.com>
2024-09-30 16:14:36 -0300

    partial

--- lua/stok/plugins/formatter.lua
+++ lua/stok/plugins/formatter.lua
@@ -1,38 +1,32 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local conform = require("conform")
 
-		conform.setup({
-			formatters_by_ft = {
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescriptreact = { "prettier" },
-				svelte = { "prettier" },
-				css = { "prettier" },
-				html = { "prettier" },
-				json = { "prettier" },
-				yaml = { "prettier" },
-				markdown = { "prettier" },
-				graphql = { "prettier" },
-				liquid = { "prettier" },
-				lua = { "stylua" },
-			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			},
-		})
+        conform.setup({
+            formatters_by_ft = {
+                lua = { "stylua" },
+                svelte = {"prettierd", "prettier"},
+                astro = { { "prettierd", "prettier", stop_after_first = true } },
+                javascript = { { "prettierd", "prettier", stop_after_first = false } },
+                typescript = { { "prettierd", "prettier", stop_after_first = false } },
+                javascriptreact = { { "prettierd", "prettier", stop_after_first = false } },
+                typescriptreact =  { "prettierd", "prettier" },
+                json = { { "prettierd", "prettier", stop_after_first = true } },
+                graphql = { { "prettierd", "prettier", stop_after_first = true } },
+                html = { "htmlbeautifier" },
+                css = { { "prettierd", "prettier", stop_after_first = true } },
+                scss = { { "prettierd", "prettier", stop_after_first = true } },
+            },
+        })
 
-		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			})
-		end, { desc = "Format file or range (in visual mode)" })
-	end,
+        vim.keymap.set({ "n", "v" }, "<leader>l", function()
+            conform.format({
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 1000,
+            })
+        end, { desc = "Format file or range (in visual mode)" })
+    end,
 }
